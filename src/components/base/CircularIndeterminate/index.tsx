7216e2a7d987ded43752e4899b1528370313125f
Luu Phuc <phucluu6297@gmail.com>
2022-06-18 22:03:41 +0700

    Fix cic

--- src/components/base/CircularIndeterminate/index.tsx
+++ src/components/base/CircularIndeterminate/index.tsx
@@ -3,7 +3,17 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 export const CircularIndeterminate = () => {
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+            }}
+        >
             <CircularProgress size={24} color="primary" />
         </Box>
     );
